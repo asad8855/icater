ca0a41635f3dc64fcc1887dbcc68986fe74dde8a
Tomerhd <33877304+Tomerhd@users.noreply.github.com>
2017-12-17 19:57:02 -0500

    Up to line 100 is relevant to our system
    Beyond that is my friends' code.

--- back-end/pythonCode.py
+++ back-end/pythonCode.py
@@ -49,6 +49,56 @@ def menus(rest_id):
     else:
         error = "Sorry, no upcoming events"
         return render_template('index.html', errorUpcoming = error, message = globUser)
+     
+@app.route('/user_reg' , methods=['GET', 'POST'])
+def user_reg():
+    #grab information from register page
+    max_id = cursor.execute('select MAX(customer_id) FROM customer_data;')    
+    userid = max_id + 1
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    password = request.form['password']
+    email = request.form['email']
+    ins = 'INSERT INTO customer_data VALUES (%s,%s,%s,%s,%s)'
+    cursor.execute(ins , (userid,firstname,lastname,password,email))
+    conn.commit()
+    cursor.close()
+    return render_template('index.html')
+
+@app.route('/user_address' , methods=['GET', 'POST'])
+def user_address():
+    #grab information from register page
+    max_id = cursor.execute('select MAX(customer_id) FROM customer_address;')    
+    userid = max_id + 1
+    street_name = request.form['street_name']
+    street_number = request.form['street_number']
+    city = request.form['city']
+    state = request.form['state']
+    zip_code = request.form['zip']
+    ins = 'INSERT INTO customer_address VALUES (%s,%s,%s,%s,%s,%s)'
+    cursor.execute(ins , (userid,street_name,street_number,city,state,zip_code))
+    conn.commit()
+    cursor.close()
+    return render_template('index.html')
+
+@app.route('/user_card' , methods=['GET', 'POST'])
+def user_card():
+    #grab information from register page
+    max_id = cursor.execute('select MAX(customer_id) FROM customer_payment;')    
+    userid = max_id + 1
+    cardholder_name= request.form['cardholder_name']
+    cardholder_company = request.form['cardholder_company']
+    card_number = request.form['card_number']
+    card_csv = request.form['card_csv']
+    expiration_month = request.form['expiration_month']
+    expiration_year = request.form['expiration_year']
+    ins = 'INSERT INTO customer_payment VALUES (%s,%s,%s,%s,%s)'
+    cursor.execute(ins , (userid,cardholder_name,cardholder_company,card_number,card_csv,expiration_month,expiration_year))
+    conn.commit()
+    cursor.close()
+    return render_template('index.html')
+
+      
         
 #index execution page
 @app.route('/indexfilter'  ,  methods =['GET','POST'])
